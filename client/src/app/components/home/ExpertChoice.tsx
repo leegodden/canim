60e99e69147f556ffd266b5632faaef9496890d6
Lee Godden <lgodden@gmail.com>
2024-06-05 13:18:19 +0100

    Add New Arrivals

---
+++ client/src/app/components/home/ExpertChoice.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface ExpertChoiceProps {}
+
+const ExpertChoice: React.FC<ExpertChoiceProps> = ({}) => {
+	return <div>ExpertChoice</div>;
+};
+
+export default ExpertChoice;
---
+++ client/src/app/components/home/NewArrivals.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface NewArrivalsProps {}
+
+const NewArrivals: React.FC<NewArrivalsProps> = ({}) => {
+	return <div>NewArrivals</div>;
+};
+
+export default NewArrivals;
