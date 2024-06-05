883dc772f3d8a1e8a4cb4ddf4a5ff4714e42ba39
Lee Godden <lgodden@gmail.com>
2024-06-05 20:23:55 +0100

    Add Banner1

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import Main from './components/shared/layouts/Main';
+import Banner1 from './components/home/Banner1';
 interface pageProps {}
 
 const Home: React.FC<pageProps> = ({}) => {
 	return (
 		<>
 			<Main />
+			<main className='flex flex-col gap-y-20 w-full'>
+				<Banner1 />
+			</main>
 		</>
 	);
 };
