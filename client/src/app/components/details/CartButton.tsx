143f83def13b28413e8a980ad8fee064a5b39cd0
Lee Godden <lgodden@gmail.com>
2024-06-05 13:09:57 +0100

    Add detail pages

---
+++ client/src/app/components/details/CartButton.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface CartButtonProps {}
+
+const CartButton: React.FC<CartButtonProps> = ({}) => {
+	return <div>CartButton</div>;
+};
+
+export default CartButton;
---
+++ client/src/app/components/details/Desription.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface DesriptionProps {}
+
+const Desription: React.FC<DesriptionProps> = ({}) => {
+	return <div>Desription</div>;
+};
+
+export default Desription;
