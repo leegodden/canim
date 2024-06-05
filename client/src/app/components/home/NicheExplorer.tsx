f5c164b0b985feb0de015761af72ff22eb7e5302
Lee Godden <lgodden@gmail.com>
2024-06-05 13:19:27 +0100

    Add Niche Explorer

---
+++ client/src/app/components/home/NicheExplorer.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface NicheExplorerProps {}
+
+const NicheExplorer: React.FC<NicheExplorerProps> = ({}) => {
+	return <div>NicheExplorer</div>;
+};
+
+export default NicheExplorer;
